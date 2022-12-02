6ec896ed6abdee0dddd3f6a4f79fdd26b194afa9
joravec-svs <joravec@svsfem.cz>
2022-12-02 09:01:14 +0100

    pubsub test

--- app/routes.py
+++ app/routes.py
@@ -8,36 +8,37 @@ from app.models import VirtualData
 @app.route("/", methods=["POST"])
 def index():
     envelope = request.get_json()
-    if not envelope:
-        msg = "no Pub/Sub message received"
-        print(f"error: {msg}")
-        return f"Bad Request: {msg}", 400
+    print(envelope)
+    # if not envelope:
+    #     msg = "no Pub/Sub message received"
+    #     print(f"error: {msg}")
+    #     return f"Bad Request: {msg}", 400
 
-    if not isinstance(envelope, dict) or "attributes" not in envelope:
-        msg = "invalid Pub/Sub message format"
-        print(f"error: {msg}")
-        return f"Bad Request: {msg}", 400
+    # if not isinstance(envelope, dict) or "attributes" not in envelope:
+    #     msg = "invalid Pub/Sub message format"
+    #     print(f"error: {msg}")
+    #     return f"Bad Request: {msg}", 400
 
-    inputdata = envelope["attributes"]
-    time = envelope["publishTime"]
+    # inputdata = envelope["attributes"]
+    # time = envelope["publishTime"]
 
-    name = app.config["FMU_NAME"]
-    inputs = app.config["INPUTS"]
-    outputs = app.config["OUTPUTS"]
-    start_time = app.config["START_TIME"]
-    stop_time = app.config["STOP_TIME"]
-    step_size = app.config["STEP_SIZE"]
-    output_interval = app.config["OUTPUT_INTERVAL"]
+    # name = app.config["FMU_NAME"]
+    # inputs = app.config["INPUTS"]
+    # outputs = app.config["OUTPUTS"]
+    # start_time = app.config["START_TIME"]
+    # stop_time = app.config["STOP_TIME"]
+    # step_size = app.config["STEP_SIZE"]
+    # output_interval = app.config["OUTPUT_INTERVAL"]
     
-    fmu = Fmu(name,inputs,outputs,start_time,stop_time,step_size,output_interval)
-    inputs = list(inputdata.values())
-    fmu_results = fmu.run_fmu(inputs)
-    fmu_results["datetime"] = time
-    print(fmu_results)
-    virtualdata = VirtualData(fmu_results)
-    print(virtualdata.datetime)
-    db.session.add(virtualdata)
-    db.session.commit()
+    # fmu = Fmu(name,inputs,outputs,start_time,stop_time,step_size,output_interval)
+    # inputs = list(inputdata.values())
+    # fmu_results = fmu.run_fmu(inputs)
+    # fmu_results["datetime"] = time
+    # print(fmu_results)
+    # virtualdata = VirtualData(fmu_results)
+    # print(virtualdata.datetime)
+    # db.session.add(virtualdata)
+    # db.session.commit()
 
     
     # if isinstance(pubsub_message, dict) and "data" in pubsub_message:
