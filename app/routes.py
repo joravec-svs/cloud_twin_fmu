90316e19ef435974bd9c907ac12fa7ee8c79bc45
joravec-svs <joravec@svsfem.cz>
2022-12-02 14:50:07 +0100

    json loads

--- app/routes.py
+++ app/routes.py
@@ -1,6 +1,7 @@
 from datetime import datetime
 from flask import request
 import base64
+import json
 
 from app import app,db
 from app.fmu import Fmu
@@ -23,27 +24,27 @@ def index():
     pubsub_message = envelope["message"]
     if isinstance(pubsub_message, dict) and "data" in pubsub_message:
         data = base64.b64decode(pubsub_message["data"]).decode("utf-8").strip()
-    print(data)
-    # inputdata = data["attributes"]
-    # time = data["timestamp"]
+    data = json.loads(data)
+    inputdata = data["attributes"]
+    time = data["timestamp"]
 
-    # name = app.config["FMU_NAME"]
-    # inputs = app.config["INPUTS"]
-    # outputs = app.config["OUTPUTS"]
-    # start_time = app.config["START_TIME"]
-    # stop_time = app.config["STOP_TIME"]
-    # step_size = app.config["STEP_SIZE"]
-    # output_interval = app.config["OUTPUT_INTERVAL"]
+    name = app.config["FMU_NAME"]
+    inputs = app.config["INPUTS"]
+    outputs = app.config["OUTPUTS"]
+    start_time = app.config["START_TIME"]
+    stop_time = app.config["STOP_TIME"]
+    step_size = app.config["STEP_SIZE"]
+    output_interval = app.config["OUTPUT_INTERVAL"]
     
-    # fmu = Fmu(name,inputs,outputs,start_time,stop_time,step_size,output_interval)
-    # inputs = list(inputdata.values())
-    # fmu_results = fmu.run_fmu(inputs)
-    # fmu_results["datetime"] = time
-    # print(fmu_results)
-    # virtualdata = VirtualData(fmu_results)
-    # print(virtualdata.datetime)
-    # db.session.add(virtualdata)
-    # db.session.commit()
+    fmu = Fmu(name,inputs,outputs,start_time,stop_time,step_size,output_interval)
+    inputs = list(inputdata.values())
+    fmu_results = fmu.run_fmu(inputs)
+    fmu_results["datetime"] = time
+    print(fmu_results)
+    virtualdata = VirtualData(fmu_results)
+    print(virtualdata.datetime)
+    db.session.add(virtualdata)
+    db.session.commit()
 
     
     if isinstance(pubsub_message, dict) and "data" in pubsub_message:
